61f12b201e398b8b5beb7a88604735c77a43bbc4
ayoub <vodkaayoub1@gmail.com>
2023-10-24 09:30:47 +0100

    fix nav mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+
+// mobile nav
+const hamburger = document.querySelector(".hamburger");
+const Nav = document.querySelector(".mobile_nav");
+
+hamburger.addEventListener("click", () => {
+  Nav.classList.toggle("mobile_nav_hide");
+});
+
+const AddToCart = document.querySelectorAll(".add_to_cart");
+
+AddToCart.forEach((button) => {
+  button.addEventListener("click", () => {
+    const id = button.getAttribute("data-id");
+    const title = button.getAttribute("data-title");
+    const image = button.getAttribute("data-image");
+    const price = button.getAttribute("data-price");
+
+    const cartItem = { id, title, image, price };
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push(cartItem);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  });
+});
+
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 10,
@@ -55,26 +81,3 @@ ScrollReveal().reveal(".footer", {
   delay: 100,
 });
 
-// mobile nav
-const hamburger = document.querySelector(".hamburger");
-const Nav = document.querySelector(".mobile_nav");
-
-hamburger.addEventListener("click", () => {
-  Nav.classList.toggle("mobile_nav_hide");
-});
-
-const AddToCart = document.querySelectorAll(".add_to_cart");
-
-AddToCart.forEach((button) => {
-  button.addEventListener("click", () => {
-    const id = button.getAttribute("data-id");
-    const title = button.getAttribute("data-title");
-    const image = button.getAttribute("data-image");
-    const price = button.getAttribute("data-price");
-
-    const cartItem = { id, title, image, price };
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push(cartItem);
-    localStorage.setItem("cart", JSON.stringify(cart));
-  });
-});
